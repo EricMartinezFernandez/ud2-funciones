8a57a7ee32e67530db16327264b5be700bf4d085
Unknown <Eric.Mtz.DAM@gmail.com>
2017-11-08 19:00:25 +0100

    Ejercicio 4 Convertir temperatura

---
+++ 04_convertir_temperatura/04_Convertir_Temperatura/src/com/Billy/Main.java
@@ -0,0 +1,25 @@
+package com.Billy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static double convertirTemperatura(double centigrados) {
+        double respuesta = centigrados * 33.8;
+        return respuesta;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Introduzca una temperatura en Grados centígrados: ");
+        double centigrados = Double.parseDouble(br.readLine());
+
+        double respuesta = convertirTemperatura(centigrados);
+
+        System.out.print("La respuesta es:  " + respuesta + " ºF");
+    }
+}
